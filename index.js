df78a5eb8739e4ac5365fec0791fc3e0b4fdafab
tusher112 <mdtusherahmed820@gmail.com>
2022-05-24 15:01:58 +0600

    database connected

--- index.js
+++ index.js
@@ -10,7 +10,19 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jdfwn.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log("mongodb connected");
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        console.log("database con")
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('hello from hexashop!')
