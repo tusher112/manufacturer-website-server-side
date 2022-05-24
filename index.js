d4187a9bb900a0c04e743168c23403d1e7e846ee
tusher112 <mdtusherahmed820@gmail.com>
2022-05-24 14:28:49 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jdfwn.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log("mongodb connected");
 
 app.get('/', (req, res) => {
   res.send('hello from hexashop!')
