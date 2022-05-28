15b211d198b8b4e9c383030cc22afe80cd1950f8
tusher112 <mdtusherahmed820@gmail.com>
2022-05-29 02:21:26 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
@@ -12,17 +13,44 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+
 async function run(){
     try{
         await client.connect();
         console.log("database con")
         const productsCollection=client.db('hexa-shop').collection('products');
+        
+        
         app.get('/product', async (req, res) =>{
           const query={};
           const cursor=productsCollection.find(query);
           const products=await cursor.toArray();
           res.send(products);
-        })
+        });
+
+
+        app.get('/user', verifyJWT, async (req, res) => {
+          const users = await userCollection.find().toArray();
+          res.send(users);
+          console.log("jwr")
+        });
+
+
     }
     finally{
 
